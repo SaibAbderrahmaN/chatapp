8cfd73659c6e68b629f7aa16f474b3a34669cc2d
abderrahmane <saibabderrahman@gmail.com>
2023-02-25 10:32:53 +0100

    first one

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -19,7 +19,7 @@ export default function Chat() {
     connectToWs();
   }, [selectedUserId]);
   function connectToWs() {
-    const ws = new WebSocket('ws:https://dead-pink-earthworm-sock.cyclic.app');
+    const ws = new WebSocket('wss:https://dead-pink-earthworm-sock.cyclic.app');
     setWs(ws);
     ws.addEventListener('message', handleMessage);
     ws.addEventListener('close', () => {
